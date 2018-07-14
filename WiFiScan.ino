d9bfed96addf12d560833dcceb1ec25fa17f8527
samagnatalanki <31427759+samagnatalanki@users.noreply.github.com>
2018-07-14 23:28:27 +0530

    Add files via upload
    Automatically connects ESP8266 to given mobile hotspot

---
+++ WiFiScan.ino
@@ -0,0 +1,39 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiAP.h>
+#include <ESP8266WiFiGeneric.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WiFiScan.h>
+#include <ESP8266WiFiSTA.h>
+#include <ESP8266WiFiType.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFiServer.h>
+#include <WiFiServerSecure.h>
+#include <WiFiUdp.h>
+
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "5d996d0c8fbf44abbe5952d4fdc4409b";
+//char auth[]= "99e66ad524ef42ceaf9b21b50dd5b4ec";
+
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "iPhone"; //your hotspot name
+char pass[] = "magima123"; //your password
+
+void setup()
+{
+  Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
